19a682c29b1fbd888fb2205424262c834455a602
tipsyelifant <deirepromissio@gmail.com>
2025-07-26 14:07:31 +0000

    Update hint container size for puzzle 1 and 2. Added back button for puzzle 3 main to go back to puzzle 3 intro

--- script.js
+++ script.js
@@ -157,6 +157,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    document.getElementById('puzzle-3-back-btn').addEventListener('click', () => {
+            document.getElementById('puzzle-3-intro-screen').style.display = 'none';
+            document.getElementById('puzzle-3-did-you-know-screen').style.display = 'flex';
+    });
+
     document.getElementById('puzzle-3-did-you-know-back-btn').addEventListener('click', () => {
         document.getElementById('puzzle-3-did-you-know-screen').style.display = 'none';
         document.getElementById('puzzle-3-main-screen').style.display = 'flex';
