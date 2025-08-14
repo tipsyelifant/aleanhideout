8f1b70154b99db7e95255421a53d8ee305fd1d2b
tipsyelifant <deirepromissio@gmail.com>
2025-08-14 22:23:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -119,13 +119,13 @@ document.addEventListener('DOMContentLoaded', () => {
       // Puzzle 2 Did You Know → VSM screen
     document.getElementById('puzzle-2-did-you-know-next-btn').addEventListener('click', function () {
         document.getElementById('puzzle-2-did-you-know-screen').style.display = 'none';
-        document.getElementById('puzzle-2-vsm-screen').style.display = 'flex';
+        document.getElementById('puzzle-2-vsm-screen').style.display = 'block';
     });
     
     // Puzzle 2 VSM Back → Puzzle 2 Did You Know
     document.getElementById('puzzle-2-vsm-back-btn').addEventListener('click', function () {
         document.getElementById('puzzle-2-vsm-screen').style.display = 'none';
-        document.getElementById('puzzle-2-did-you-know-screen').style.display = 'flex';
+        document.getElementById('puzzle-2-did-you-know-screen').style.display = 'block';
     });
     
     // Puzzle 2 VSM Next → Puzzle 3 Story
